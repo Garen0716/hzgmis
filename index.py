abf217fd8b996eb65a9a521af8bb5f6b6d2c25b8
karl1357 <yankai930530@gmail.com>
2023-10-24 21:48:58 +0800

    make it better

--- index.py
+++ index.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    homepage = "<h1>楊子青Python網頁</h1>"
+    homepage = "<h1>黃子鑒Python網頁</h1>"
     homepage += "<a href=/mis>MIS</a><br>"
     homepage += "<a href=/today>顯示日期時間</a><br>"
-    homepage += "<a href=/welcome?nick=tcyang>傳送使用者暱稱</a><br>"
-    homepage += "<a href=/about>子青簡介網頁</a><br>"
+    homepage += "<a href=/welcome?nick=ZGHUANG>傳送使用者暱稱</a><br>"
+    homepage += "<a href=/about>子鑒簡介網頁</a><br>"
     return homepage
 
 @app.route("/mis")
