5d1ceb51c44d1146c82cb21af1de70cbc21a5d26
Garen0716 <hzg930716@gmail.com>
2023-11-07 22:46:04 +0800

    4

--- index.py
+++ index.py
@@ -40,6 +40,14 @@ def account():
         return result
     else:
         return render_template("account.html")
+@app.route("/read")
+def read():
+    Result = ""     
+    collection_ref = db.collection("人選之人─造浪者")    
+    docs = collection_ref.order_by("name", direction=firestore.Query.DESCENDING).get()    
+    for doc in docs:         
+        Result += "文件內容：{}".format(doc.to_dict()) + "<br>"    
+    return Result
 
 #if __name__ == "__main__":
     #app.run()
