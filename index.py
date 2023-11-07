3965f3799bfd8ad7f6cd67c376dd55f6f3a4e8e3
Garen0716 <hzg930716@gmail.com>
2023-11-07 22:54:27 +0800

    4.2

--- index.py
+++ index.py
@@ -1,13 +1,7 @@
-import firebase_admin
-from firebase_admin import credentials, firestore
-cred = credentials.Certificate("serviceAccountKey.json")
-firebase_admin.initialize_app(cred)
-db = firestore.client()
-
-
 from flask import Flask, render_template, request
-
 from datetime import datetime, timezone, timedelta
+app = Flask(__name__)
+
 @app.route("/")
 def index():
     homepage = "<h1>黃子鑒Python網頁</h1>"
